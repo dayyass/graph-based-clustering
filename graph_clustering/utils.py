6f0537e8f205fa5648aca9f73d7282e6bd2306b1
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-16 21:17:51 +0300

    minor changes

--- graph_clustering/utils.py
+++ graph_clustering/utils.py
@@ -34,28 +34,28 @@ def _check_square_matrix_is_symmetric(
     rtol: float = 1e-05,
     atol: float = 1e-08,
 ) -> bool:
-    """Check if a matrix is symmetric.
+    """Check if a square matrix is symmetric.
 
     Args:
-        a (np.ndarray): A matrix to check.
+        a (np.ndarray): A square matrix to check.
         rtol (float, optional): The relative tolerance parameter. Defaults to 1e-05.
         atol (float, optional): The absolute tolerance parameter. Defaults to 1e-08.
 
     Returns:
-        bool: A matrix is symmetric.
+        bool: A square matrix is symmetric.
     """
 
     return np.allclose(a, a.T, rtol=rtol, atol=atol)
 
 
 def check_symmetric(distances: np.ndarray) -> bool:
-    """Check if a distances matrix is correct.
+    """Check if a matrix is symmetric.
 
     Args:
-        distances (np.ndarray): A distances matrix.
+        distances (np.ndarray): A matrix to check.
 
     Returns:
-        bool: A distances matrix is correct.
+        bool: A matrix is symmetric.
     """
 
     is_matrix = _check_matrix(distances)
