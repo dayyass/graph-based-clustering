141464e757459d709fdb5214f52568d521cadeab
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-16 21:48:16 +0300

    add distances_to_adjacency_matrix

--- graph_clustering/utils.py
+++ graph_clustering/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from check import _check_matrix
+from check import _check_matrix, check_symmetric
 from sklearn.metrics import pairwise_distances
 
 
@@ -24,3 +24,26 @@ def _pairwise_distances(
     distances = pairwise_distances(X=X, metric=metric, n_jobs=n_jobs)
 
     return distances
+
+
+def distances_to_adjacency_matrix(
+    distances: np.ndarray,
+    threshold: float,
+) -> np.ndarray:
+    """Convert a pairwise distance matrix to adjacency_matrix given threshold.
+
+    Args:
+        distances (np.ndarray): A pairwise distance matrix.
+        threshold (float): threshold to make grapg edges.
+
+    Returns:
+        np.ndarray: The adjacency_matrix.
+    """
+
+    assert check_symmetric(distances)
+
+    N = distances.shape[0]
+
+    adjacency_matrix = (distances < threshold).astype(int) - np.eye(N)
+
+    return adjacency_matrix
