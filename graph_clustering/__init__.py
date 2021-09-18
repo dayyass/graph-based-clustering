b43b987542deaef35b913c0542513bb6072bae23
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-18 22:10:12 +0300

    add SpanTreeConnectedComponentsClustering

--- graph_clustering/__init__.py
+++ graph_clustering/__init__.py
@@ -1,6 +1,7 @@
-from .main import ConnectedComponentsClustering
+from .main import ConnectedComponentsClustering, SpanTreeConnectedComponentsClustering
 
 __version__ = "0.1.0"
 __all__ = [
     "ConnectedComponentsClustering",
+    "SpanTreeConnectedComponentsClustering",
 ]
