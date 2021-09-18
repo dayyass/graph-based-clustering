483bfea22c37afd002008f886ea10417515a7265
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-18 22:09:18 +0300

    add SpanTreeConnectedComponentsClustering

--- graph_clustering/main.py
+++ graph_clustering/main.py
@@ -2,10 +2,14 @@ from typing import Callable, Optional, Union
 
 import numpy as np
 from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import connected_components
+from scipy.sparse.csgraph import connected_components, minimum_spanning_tree
 from sklearn.base import BaseEstimator, ClusterMixin
 
-from .utils import _pairwise_distances, distances_to_adjacency_matrix
+from .utils import (
+    _pairwise_distances,
+    distances_to_adjacency_matrix,
+    span_tree_top_n_weights_idx,
+)
 
 
 class ConnectedComponentsClustering(ClusterMixin, BaseEstimator):
@@ -74,6 +78,69 @@ class ConnectedComponentsClustering(ClusterMixin, BaseEstimator):
         Args:
             X (np.ndarray): A matrix.
         """
+
+        self.fit(X)
+        return self.labels_
+
+
+class SpanTreeConnectedComponentsClustering(ClusterMixin, BaseEstimator):
+
+    """TODO"""
+
+    def __init__(
+        self,
+        n_clusters: int,
+        metric: Union[str, Callable] = "euclidean",
+        n_jobs: Optional[int] = None,
+    ) -> None:
+        """TODO"""
+
+        self.n_clusters = n_clusters
+        self.metric = metric
+        self.n_jobs = n_jobs
+
+    def fit(self, X: np.ndarray):
+        """TODO"""
+
+        X = self._validate_data(X, accept_sparse="csr")
+
+        distances = _pairwise_distances(
+            X=X,
+            metric=self.metric,
+            n_jobs=self.n_jobs,
+        )
+
+        span_tree = minimum_spanning_tree(distances).toarray()
+
+        top_n_weights_idx = span_tree_top_n_weights_idx(
+            span_tree=span_tree,
+            n=self.n_clusters - 1,
+        )
+
+        graph_n_clusters = span_tree.copy()
+
+        graph_n_clusters[top_n_weights_idx] = 0
+        graph_n_clusters[graph_n_clusters > 0] = 1
+        graph_n_clusters = graph_n_clusters.astype(int)
+
+        graph = csr_matrix(graph_n_clusters)
+
+        n_components, labels = connected_components(
+            csgraph=graph,
+            directed=False,
+            return_labels=True,
+        )
+
+        self.components_ = n_components
+        self.labels_ = labels
+
+        return self
+
+    def fit_predict(
+        self,
+        X: np.ndarray,
+    ):
+        """TODO"""
 
         self.fit(X)
         return self.labels_
