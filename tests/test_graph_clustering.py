a1f7adb2ceba0d836f135b0c9e20eab187286513
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-18 18:15:47 +0300

    add parametrized tests

--- tests/test_graph_clustering.py
+++ tests/test_graph_clustering.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+from parameterized import parameterized_class
 
 from graph_clustering.check import check_adjacency_matrix, check_symmetric
 from graph_clustering.main import GraphConnectedComponentsClustering
@@ -9,6 +10,8 @@ from graph_clustering.utils import _pairwise_distances, distances_to_adjacency_m
 X = np.array([[0, 1], [1, 0], [1, 1]])
 
 distances = _pairwise_distances(X)
+
+print(distances)
 
 adjacency_matrix = distances_to_adjacency_matrix(
     distances=distances,
@@ -36,20 +39,28 @@ class TestCheck(unittest.TestCase):
         self.assertTrue(check_adjacency_matrix(adjacency_matrix))
 
 
+@parameterized_class(
+    [
+        {"threshold": 1.5, "components_": 1, "labels_": [0, 0, 0]},
+        {"threshold": 1.25, "components_": 1, "labels_": [0, 0, 0]},
+        {"threshold": 1.0, "components_": 3, "labels_": [0, 1, 2]},
+        {"threshold": 0.75, "components_": 3, "labels_": [0, 1, 2]},
+    ]
+)
 class TestClustering(unittest.TestCase):
     def test_GraphConnectedComponentsClustering(self):
         """Test GraphConnectedComponentsClustering"""
 
         clustering = GraphConnectedComponentsClustering(
-            threshold=1.25,
+            threshold=self.threshold,
             metric="euclidean",
             n_jobs=-1,
         )
 
         clustering.fit(X)
 
-        self.assertEqual(clustering.components_, 1)
-        self.assertTrue(np.allclose(clustering.labels_, [0, 0, 0]))
+        self.assertEqual(clustering.components_, self.components_)
+        self.assertTrue(np.allclose(clustering.labels_, self.labels_))
 
         labels = clustering.fit_predict(X)
 
